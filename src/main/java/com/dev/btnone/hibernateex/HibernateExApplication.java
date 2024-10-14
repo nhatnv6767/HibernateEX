5e6b2c99f102b4117a6010c524cfb183467cf6f5
bhnone <>
2024-10-14 22:56:24 +0900

    first commit

---
+++ src/main/java/com/dev/btnone/hibernateex/HibernateExApplication.java
@@ -0,0 +1,13 @@
+package com.dev.btnone.hibernateex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/btnone/hibernateex/HibernateExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.btnone.hibernateex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
