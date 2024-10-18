1a1214b27337ec58328bd7c20a25bbc3d76a51f9
bhnone <>
2024-10-18 23:00:37 +0900

    one - to - many

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,60 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Customer {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+
+    // dua tren thang customer, customer ra doi truoc, roi moi sinh ra thang order
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/model/Order.java
@@ -0,0 +1,49 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Order {
+    @Id
+    private String order_id;
+    private String address;
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    public Order() {
+    }
+
+    public Order(String order_id, String address, Customer customer) {
+        this.order_id = order_id;
+        this.address = address;
+        this.customer = customer;
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
